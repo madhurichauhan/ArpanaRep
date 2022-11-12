aed37bf3245839c975c43a0ecd8259cffdcf7e20
Arpana <muriki.arpana@gmail.com>
2022-11-12 05:36:00 +0530

    Third Commit

---
+++ src/main/java/stepDefinations/TestCase2.java
@@ -0,0 +1,10 @@
+package stepDefinations;
+
+public class TestCase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my TestCase 2");
+	}
+
+}
